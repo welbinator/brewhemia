b44033b7ad962d76dea18b0ddce33c0e7f1461b3
James <james.welbes@gmail.com>
2020-04-03 08:34:46 -0500

    added events page

---
+++ events.php
@@ -0,0 +1,15 @@
+<?php include "includes/header.php" ?>
+    <section class="about-hero">
+        
+        <h2>Events</h2>
+
+    </section>
+    <section class="about">
+        <div class="container">
+            <div class="about-inner">
+               <h3 style="color: black;">No events at this time</h3>
+            </div>
+        </div>
+    </section>
+
+    <?php include "includes/footer.php" ?>
