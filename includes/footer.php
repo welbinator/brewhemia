33a28da498c9cc51e6318607d0087e43b85d7258
James <james.welbes@gmail.com>
2020-04-04 01:41:06 -0500

    htaccess

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
 <footer>
         <div class="container">
+
+        <div class="footer-section">
         <div class="hours">
             <h3>HOURS</h3>
             <div class="hour"><div class="day">Mon - Thurs</div><div class="time">7am-6pm</div></div>
@@ -7,26 +9,33 @@
             <div class="hour"><div class="day">Sat</div><div class="time">8am-6pm</div></div>
             <div class="hour"><div class="day">Sun</div><div class="time">8am-5pm</div></div>
         </div>
+        </div><!-- footer-section -->
+        <div class="footer-section">
         <div class="connect">
             <h3>CONNECT</h3>
             <div class="connect-inner"> 
-                <p>Facebook: <a target="_blank" href="https://web.archive.org/web/20191001193057/https://www.facebook.com/Brewhemia">Like our page!</a></p>
-                <p>Email: info@brewhemia.com </p>
-                <p>Phone: (319) 364-0802</p>
+                <p><i class="fa fa-facebook-square"></i> <a target="_blank" href="https://web.archive.org/web/20191001193057/https://www.facebook.com/Brewhemia">Like our page!</a></p>
+                <p><i class="fa fa-envelope"></i> info@brewhemia.com </p>
+                <p><img src="../assets/images/mobile.svg" style="color: white; height: 25px;"></i> (319) 364-0802</p>
             </div>
             <a class="online" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">ORDER ONLINE</a>
             <a class="online" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101/giftcards">BUY GIFT CARDS</a>
         </div>
+        </div><!-- footer-section -->
+        <div class="footer-section">
         <div class="address">
             <h3>ADDRESS</h3>
             <p>1202 3rd St SE</p>
             <p>Cedar Rapids, Iowa 52401</p>
             <p><a href="https://www.google.com/maps/dir//1202+3rd+Street+Southeast,+Cedar+Rapids,+IA+52401/@41.9703114,-91.6927482,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x87e4f7408d72e2bd:0x235ce8871a6ba327!2m2!1d-91.6577287!2d41.9702529">Get Directions</a></p>
         </div>
+        </div><!-- footer-section -->
     </div><!-- container -->
     <div class="copyright"><p>Copyright © 2019 <strong>BREWHEMIA.</strong> All Rights Reserved.</p></div>
     </footer>
 
+    <script src="https://use.fontawesome.com/3a535ab4da.js"></script>
     <script src="assets/js/script.js"></script>
+
 </body>
 </html>
