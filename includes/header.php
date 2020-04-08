cde309b0b0f8057b1f7ab7e51a7febe97bc74022
James <james.welbes@gmail.com>
2020-04-07 19:20:35 -0500

    replaced analytics with gta

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NQWLQ76');</script>
-    <!-- End Google Tag Manager -->
+<?php include "includes/analytics.php" ?>
+   
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,7 +14,7 @@
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
     <title>Document</title>
 
-    <?php include "includes/analytics.php" ?>
+   
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
