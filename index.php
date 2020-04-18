797aae202ef93badc781d80bb66a0534552e757a
James <james.welbes@gmail.com>
2020-04-18 13:00:03 -0500

    added ad image

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <?php include "includes/header.php" ?>
+    
+
     <section class="hero">
         <img src="assets/images/brewhemia-hero.jpg">
-
+    </section>
+        
+    <section class="ad">
+    <img src="/assets/images/brewhemia-free-tp.png" alt="">
     </section>
     <section class="covid">
-<?php include "includes/covid.php" ?>
-</section>
-    <section class="features">
-    
-        <div class="container">
-
-
-
-        <div class="features-container">
-            <div class="feature"><img src="assets/images/bottles-dark.jpg"><h2><a href="/about-us">ABOUT US</a></h2></div>
-            <div class="feature"><img src="assets/images/grilled-cheese-and-soup-thumb.jpg"><h2><a href="/menu">MENU</a></h2></div>
-            <div class="feature"><img src="assets/images/vinyl-night3.jpg"><h2><a href="/events">EVENTS</a></h2></div>
-        </div>
-    </div><!-- container -->
+        <?php include "includes/covid.php" ?>
     </section>
-    <section class="tagline"><p>COFFEE • ESPRESSO • BEER • WINE • SPIRITS • FOOD</p></section>
+
+    <section class="features">
+        <div class="container">
+            <div class="features-container">
+             <div class="feature"><img src="assets/images/bottles-dark.jpg"><h2><a href="/about-us">ABOUT US</a></h2></div>
+             <div class="feature"><img src="assets/images/grilled-cheese-and-soup-thumb.jpg"><h2><a href="/menu">MENU</a></h2></div>
+             <div class="feature"><img src="assets/images/vinyl-night3.jpg"><h2><a href="/events">EVENTS</a></h2></div>
+            </div>
+        </div><!-- container -->
+    </section>
+
+    <section class="tagline">
+        <p>COFFEE • ESPRESSO • BEER • WINE • SPIRITS • FOOD</p>
+    </section>
+
     <?php include "includes/footer.php" ?>
