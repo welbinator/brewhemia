2d4771d7de7ca0eda7e8f9201ffaea871b774c7c
James <james.welbes@gmail.com>
2020-05-13 12:13:45 -0500

    added copy about catering and cahnged font sizes

--- contact-us.php
+++ contact-us.php
@@ -7,8 +7,9 @@
     <section class="contact">
         <div class="container">
             <div class="contact-inner">
-                <h3>Open Ears!</h3>
-                <p>We would love to hear your thoughts, ideas, criticisms, or ramblings. Send us a note and we’ll direct it to the right member of our tribe.</p>
+                <h3 class="dark">Open Ears!</h3>
+                <p class="dark">We would love to hear your thoughts, ideas, criticisms, or ramblings. Send us a note and we’ll direct it to the right member of our tribe.</p>
+                <p class="dark">Want to talk catering? Call/text Andrea at 319-389-3595 or fill out the form below!</p>
                 <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
                 <script>
                 hbspt.forms.create({
--- menu.php
+++ menu.php
@@ -5,7 +5,9 @@
 
     </section>
     <section class="menu">
-        <div class="container">
+        <div class="container" style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+            <h2 class="dark" style="line-height: 40px;">Want to talk Catering, Coffee, Breakfast or Lunch for your office or event? Call/Text Andrea at <a style="text-decoration: none;color:black;" href="tel:3193893595">319-389-3595</a> or email info@brewhemia.com</h2>
+            <br>
             <a class="online" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">ORDER ONLINE</a>
         </div>
     </section>
