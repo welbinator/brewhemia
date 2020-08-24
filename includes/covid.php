26de9a4536b314612c85e3ae1c2dd69fae3af121
James Welbes <james.welbes@gmail.com>
2020-08-24 09:52:13 -0500

    update hours

--- includes/covid.php
+++ includes/covid.php
@@ -1,86 +1,101 @@
 <div class="covid-inner">
-<h3>ARE YOU SERIOUSLY OFFERING A FREE ROLL OF TP WITH EVERY ORDER OVER $25?</h3><br>
+    <h3>ARE YOU SERIOUSLY OFFERING A FREE ROLL OF TP WITH EVERY ORDER OVER $25?</h3><br>
 
-<p class="bold">YES… More on that in a second…</p><br><hr>
+    <p class="bold">YES… More on that in a second…</p><br>
+    <hr>
 
-<p class="bold">When Are You Open?</p>
-<p>8-2 Wed-Sunday</p>
-<p>Easy No Contact Carry Out & Delivery</p><br><hr>
+    <p class="bold">When Are You Open?</p>
+    <p>8-2 Every day.</p>
+    <p>Easy No Contact Carry Out & Delivery</p><br>
+    <hr>
 
-<p class="bold">What can I order?</p>
-<p>Breakfast and lunch and limited coffee beverages.   </p>
-<p>Our breakfast burrito was recognized as one of the best in the nation by Food Network.</p><br><hr>
+    <p class="bold">What can I order?</p>
+    <p>Breakfast and lunch and limited coffee beverages. </p>
+    <p>Our breakfast burrito was recognized as one of the best in the nation by Food Network.</p><br>
+    <hr>
 
-<p class="bold">What are the specials?</p>
+    <p class="bold">What are the specials?</p>
 
-<p>Keep and eye out for our Friday Specials!</p><br><hr>
+    <p>Keep and eye out for our Friday Specials!</p><br>
+    <hr>
 
-<p class="bold">Where do I order?</p>
-<p><strong>CLICK HERE:</strong> <a id="where-to-order" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101</a></p><br><hr>
+    <p class="bold">Where do I order?</p>
+    <p><strong>CLICK HERE:</strong> <a id="where-to-order" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101</a></p><br>
+    <hr>
 
-<p class="bold">Want to Schedule Your Meal for Wednesday at NOON? WE CAN DO THAT!</p>
-<p><strong>CLICK HERE:</strong> <a id="schedule-order" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101</a></p><br><hr>
+    <p class="bold">Want to Schedule Your Meal for Wednesday at NOON? WE CAN DO THAT!</p>
+    <p><strong>CLICK HERE:</strong> <a id="schedule-order" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101</a></p><br>
+    <hr>
 
-<p class="bold">I want to send a gift to a pal, do you have gift cards?</p>
-<p class="bold">Yes! And they are digitally delivered, so your friend can use it instantly! </p>
-<p><a id="gift-cards" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101/giftcards">https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101/giftcards</a></p><br><hr>
+    <p class="bold">I want to send a gift to a pal, do you have gift cards?</p>
+    <p class="bold">Yes! And they are digitally delivered, so your friend can use it instantly! </p>
+    <p><a id="gift-cards" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101/giftcards">https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101/giftcards</a></p><br>
+    <hr>
 
-<p class="bold">How Do I Order?</p><br>
-<ol>
-<li>Online: <a id="how-to-order" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101</a></li>
+    <p class="bold">How Do I Order?</p><br>
+    <ol>
+        <li>Online: <a id="how-to-order" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101</a></li>
 
-<li>Email larger orders in advance to info@brewhemia.com</li>
-<li>Message on Facebook: <a href="https://www.facebook.com/Brewhemia">https://www.facebook.com/Brewhemia</a></li>
-</ol><br><hr>
+        <li>Email larger orders in advance to info@brewhemia.com</li>
+        <li>Message on Facebook: <a href="https://www.facebook.com/Brewhemia">https://www.facebook.com/Brewhemia</a></li>
+    </ol><br>
+    <hr>
 
-<p class="bold">Do you still offer catering?  </p>
-<p>Yes, we can still deliver breakfast or boxed lunches to your office! Email info@brewhemia.com</p><br><hr>
+    <p class="bold">Do you still offer catering? </p>
+    <p>Yes, we can still deliver breakfast or boxed lunches to your office! Email info@brewhemia.com</p><br>
+    <hr>
 
 
 
-<p class="bold">What are precautions you are taking?</p>
-<p>Sanitizing all surfaces immediately after use and in regular intervals throughout the day.</p>
-<p>Gloves/Masks internally. </p>
-<p>Minimal staffing (3 employees maximum).  </p>
-<p>No contact sanitized vestibule pick up with hand sanitizer dispenser upon entry.  </p><br><hr>
+    <p class="bold">What are precautions you are taking?</p>
+    <p>Sanitizing all surfaces immediately after use and in regular intervals throughout the day.</p>
+    <p>Gloves/Masks internally. </p>
+    <p>Minimal staffing (3 employees maximum). </p>
+    <p>No contact sanitized vestibule pick up with hand sanitizer dispenser upon entry. </p><br>
+    <hr>
 
-<!-- <p class="bold">What's with the current limited menu?</p>
+    <!-- <p class="bold">What's with the current limited menu?</p>
 <p>We currently don’t have a barista staffed during the day but have a few bulk coffee drinks in half gallons.  </p>
 <p>We were fully closed for a couple weeks and reopened our  kitchen to serve you.  All of this is new and changes all the time!</p><br><hr> -->
 
-<p class="bold">What are the protocols for pick up?  </p>
-<p>Pull up to front door on 3rd Street. Enter the vestibule (the door is already open) and grab the bag with your name on it.  Give us a wave and a smile through the glass :) </p><br><hr>
+    <p class="bold">What are the protocols for pick up? </p>
+    <p>Pull up to front door on 3rd Street. Enter the vestibule (the door is already open) and grab the bag with your name on it. Give us a wave and a smile through the glass :) </p><br>
+    <hr>
 
-<!-- <p class="bold">What about drink orders?  </p>
+    <!-- <p class="bold">What about drink orders?  </p>
 <p>Temporarily only by the half gallon (enough to get your brewhemia fix for a couple days) </p><br><hr> -->
 
-<p class="bold">Are there other fees?</p>
-<p>Only for delivery</p><br><hr>
+    <p class="bold">Are there other fees?</p>
+    <p>Only for delivery</p><br>
+    <hr>
 
-<p class="bold">Should I tip?</p>
-<p>Yes please :)</p><br><hr>
+    <p class="bold">Should I tip?</p>
+    <p>Yes please :)</p><br>
+    <hr>
 
-<p class="bold">Why is it important that Brewhemia stay open? Why not close and wait it out?</p>
-<p>Reality: We have bills to pay.  We also really really want to stay connected to our customers. </p><br><hr>
+    <p class="bold">Why is it important that Brewhemia stay open? Why not close and wait it out?</p>
+    <p>Reality: We have bills to pay. We also really really want to stay connected to our customers. </p><br>
+    <hr>
 
-<!-- <p class="bold">Bulk Coffee Drinks: </p>
+    <!-- <p class="bold">Bulk Coffee Drinks: </p>
 <p>Without a barista consistently on the schedule we still make a mean line of Cold Brew beverages”: Classic cold brew, Thai coffee and our famous Bohemian are offered in an Economy size for you to enjoy from your place of confinement. </p> <br><hr>  -->
 
-<!-- <p class="bold">About Single Serving Coffee</p>
+    <!-- <p class="bold">About Single Serving Coffee</p>
 <p class="bold">We currently aren’t doing single coffee beverages for a couple of reasons: </p>
     <ul>
     <li>our baristas are staying safe at home.</li>
     <li>B)  We are trying to reduce the number of small transactions to reduce touch points.  </li>
     </ul><br><hr> -->
 
-<p class="bold">Are you giving away toilet paper?</p>
-<p>Yes, there is a bin in the front. If your order is over $25, grab a roll!</p><br><hr>
+    <p class="bold">Are you giving away toilet paper?</p>
+    <p>Yes, there is a bin in the front. If your order is over $25, grab a roll!</p><br>
+    <hr>
 
     <p>Sincere Thank You, <br>
 
-    <p>Steve & Andrea Shriver, Matt Danielson and the entire Brew Crew. </p><br>
+        <p>Steve & Andrea Shriver, Matt Danielson and the entire Brew Crew. </p><br>
 
-    <p>Have The Best Day! </p>
+        <p>Have The Best Day! </p>
 
 
 
--- includes/footer.php
+++ includes/footer.php
@@ -1,41 +1,47 @@
 <footer>
-        <div class="container">
+    <div class="container">
 
         <div class="footer-section">
-        <div class="hours">
-            <h3>Temporary HOURS</h3>
-            <div class="hour"><div class="day">Mon - Tues</div><div class="time">Closed</div></div>
-            <div class="hour"><div class="day">Wed - Sun</div><div class="time">8am - 2pm</div></div>
-            <!-- <div class="hour"><div class="day">Sat</div><div class="time">8am-6pm</div></div>
+            <div class="hours">
+                <h3>Temporary HOURS</h3>
+
+                <div class="hour">
+                    <div class="day">Every Day</div>
+                    <div class="time">8am - 2pm</div>
+                </div>
+                <!-- <div class="hour"><div class="day">Sat</div><div class="time">8am-6pm</div></div>
             <div class="hour"><div class="day">Sun</div><div class="time">8am-5pm</div></div> -->
-        </div>
-        </div><!-- footer-section -->
-        <div class="footer-section">
-        <div class="connect">
-            <h3>CONNECT</h3>
-            <div class="connect-inner"> 
-                <p><i class="fa fa-facebook-square"></i> <a target="_blank" href="https://www.facebook.com/Brewhemia">Like our page!</a></p>
-                <p><i class="fa fa-envelope"></i> <a target="_blank" href="mailto:info@brewhemia.com">info@brewhemia.com</a></p>
-                <p><img src="../assets/images/mobile.svg" style="color: white; height: 25px;"></i> (319) 364-0802</p>
             </div>
-            <a class="online pulsate-bck" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">ORDER ONLINE</a>
-            <a class="online" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101/giftcards">BUY GIFT CARDS</a>
-        </div>
         </div><!-- footer-section -->
         <div class="footer-section">
-        <div class="address">
-            <h3>ADDRESS</h3>
-            <p>1202 3rd St SE</p>
-            <p>Cedar Rapids, Iowa 52401</p>
-            <p><a href="https://www.google.com/maps/dir//1202+3rd+Street+Southeast,+Cedar+Rapids,+IA+52401/@41.9703114,-91.6927482,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x87e4f7408d72e2bd:0x235ce8871a6ba327!2m2!1d-91.6577287!2d41.9702529">Get Directions</a></p>
-        </div>
+            <div class="connect">
+                <h3>CONNECT</h3>
+                <div class="connect-inner">
+                    <p><i class="fa fa-facebook-square"></i> <a target="_blank" href="https://www.facebook.com/Brewhemia">Like our page!</a></p>
+                    <p><i class="fa fa-envelope"></i> <a target="_blank" href="mailto:info@brewhemia.com">info@brewhemia.com</a></p>
+                    <p><img src="../assets/images/mobile.svg" style="color: white; height: 25px;"></i> (319) 364-0802</p>
+                </div>
+                <a class="online pulsate-bck" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101">ORDER ONLINE</a>
+                <a class="online" href="https://www.toasttab.com/brewhemia-1202-3rd-st-se-ste-101/giftcards">BUY GIFT CARDS</a>
+            </div>
+        </div><!-- footer-section -->
+        <div class="footer-section">
+            <div class="address">
+                <h3>ADDRESS</h3>
+                <p>1202 3rd St SE</p>
+                <p>Cedar Rapids, Iowa 52401</p>
+                <p><a href="https://www.google.com/maps/dir//1202+3rd+Street+Southeast,+Cedar+Rapids,+IA+52401/@41.9703114,-91.6927482,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x87e4f7408d72e2bd:0x235ce8871a6ba327!2m2!1d-91.6577287!2d41.9702529">Get Directions</a></p>
+            </div>
         </div><!-- footer-section -->
     </div><!-- container -->
-    <div class="copyright"><p>Copyright © 2019 <strong>BREWHEMIA.</strong> All Rights Reserved.</p></div>
-    </footer>
+    <div class="copyright">
+        <p>Copyright © 2019 <strong>BREWHEMIA.</strong> All Rights Reserved.</p>
+    </div>
+</footer>
 
-    <script src="https://use.fontawesome.com/3a535ab4da.js"></script>
-    <script src="assets/js/script.js"></script>
+<script src="https://use.fontawesome.com/3a535ab4da.js"></script>
+<script src="assets/js/script.js"></script>
 
 </body>
+
 </html>
