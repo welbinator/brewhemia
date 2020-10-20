cc0ba2c859a9fbf7d01117985bdb096b925ef5a4
James Welbes <james.welbes@gmail.com>
2020-10-20 13:00:40 -0500

    update hero section. remove covid section

--- index.php
+++ index.php
@@ -1,30 +1,44 @@
 <?php include "includes/header.php" ?>
-    
 
-    <section class="hero">
-    <h2>Open for Carryout and Delivery</h2>
-        <img src="assets/images/brewhemia-hero.jpg">
-    </section>
-        
-    <section class="ad">
+
+<section class="hero">
+    <h2>Open For Carry Out, Deliver, and Dine in (most days) upstairs at the Olympic</h2>
+    <img src="assets/images/brewhemia-hero.jpg">
+</section>
+
+<!-- <section class="ad">
     <img src="/assets/images/brewhemia-free-tp.png" alt="">
-    </section>
-    <section class="covid">
-        <?php include "includes/covid.php" ?>
-    </section>
+</section>
+<section class="main">
+    <div class="container">
 
-    <section class="features">
-        <div class="container">
-            <div class="features-container">
-             <div class="feature"><img src="assets/images/bottles-dark.jpg"><h2><a href="/about-us">ABOUT US</a></h2></div>
-             <div class="feature"><img src="assets/images/grilled-cheese-and-soup-thumb.jpg"><h2><a href="/menu">MENU</a></h2></div>
-             <div class="feature"><img src="assets/images/vinyl-night3.jpg"><h2><a href="/events">EVENTS</a></h2></div>
+        <div class="text-group">
+            <p class="bold">When Are You Open?</p>
+            <p>8-2 Tuesday-Sunday.</p>
+            <p>Closed Mondays</p>
+            <p>Easy No Contact Carry Out & Delivery</p>
+        </div>
+    </div>
+</section> -->
+
+<section class="features">
+    <div class="container">
+        <div class="features-container">
+            <div class="feature"><img src="assets/images/bottles-dark.jpg">
+                <h2><a href="/about-us">ABOUT US</a></h2>
             </div>
-        </div><!-- container -->
-    </section>
+            <div class="feature"><img src="assets/images/grilled-cheese-and-soup-thumb.jpg">
+                <h2><a href="/menu">MENU</a></h2>
+            </div>
+            <div class="feature"><img src="assets/images/vinyl-night3.jpg">
+                <h2><a href="/events">EVENTS</a></h2>
+            </div>
+        </div>
+    </div><!-- container -->
+</section>
 
-    <section class="tagline">
-        <p>COFFEE • ESPRESSO • BEER • WINE • SPIRITS • FOOD</p>
-    </section>
+<section class="tagline">
+    <p>COFFEE • ESPRESSO • BEER • WINE • SPIRITS • FOOD</p>
+</section>
 
-    <?php include "includes/footer.php" ?>
+<?php include "includes/footer.php" ?>
